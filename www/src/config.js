384ba7542dbe592811a08d998dcd66120116abfb
Konstantin Scherer <hardworking@web.de>
2022-05-19 10:42:09 +0200

    Update config.js
    cognito config

--- www/src/config.js
+++ www/src/config.js
@@ -2,9 +2,9 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
+  "aws_user_pools_web_client_id": "4gcr1vvliggmp2h89l4uph9u2r",     // CognitoClientID
   "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
+  "cognito_hosted_domain": "kosc.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "None"                                      // AmplifyURL
 };
 
