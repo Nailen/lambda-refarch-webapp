234dca033e407553706dbcecb75c59ec5c9a6188
Konstantin <kosc@zuehlke.com>
2022-05-19 11:06:29 +0200

    cognito config

--- www/src/config.js
+++ www/src/config.js
@@ -5,7 +5,7 @@ const config = {
   "aws_user_pools_web_client_id": "4gcr1vvliggmp2h89l4uph9u2r",     // CognitoClientID
   "api_base_url": "None",                                     // TodoFunctionApi
   "cognito_hosted_domain": "kosc.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "redirect_url": "https://master.d29y886xyu30cy.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
